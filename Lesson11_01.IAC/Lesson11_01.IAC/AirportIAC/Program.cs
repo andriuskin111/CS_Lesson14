4021d512dd2ff0835bcc26d32cf422d51219af07
andriuskin111 <andriuskin@gmail.com>
2020-02-15 13:02:52 +0200

    Initiate

---
+++ Lesson11_01.IAC/Lesson11_01.IAC/AirportIAC/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Mail;
+using System.Net;
+using IAC.BL;
+using IAC.BL.Repositories;
+
+namespace AirportIAC
+{
+    class Program
+    {
+        public static NetworkCredential Credentials { get; private set; }
+        public static bool EnableSsl { get; private set; }
+
+        static void Main(string[] args)
+        {
+            ReportGenerator reportGenerator = new ReportGenerator(
+               new AircraftRepository(),
+               new AircraftModelRepository(),
+               new CompanyRepository(),
+               new CountryRepository());
+
+            HTMLFormatter hTMLFormatter =  new HTMLFormatter();
+
+            string report = hTMLFormatter.FormatHTML(reportGenerator.GenerateReportAircraftInEurope());
+
+            //Console.WriteLine(report);
+
+            var client = new SmtpClient("smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("740fca1e4b51f5", "a6e511b6f5b235"),
+                EnableSsl = true
+            };
+
+            var msg = new MailMessage(
+                "from@gmail.com",
+                "to@gmail.com",
+                "HTML Report",
+                report);
+
+            msg.IsBodyHtml = true;
+
+            client.Send(msg);
+
+            Console.WriteLine("send");
+
+            //foreach (var item in reportGenerator.GenerateReportAircraftInEurope())
+            //{
+            //    Console.WriteLine(
+            //        $"{item.AircraftTailNumber}" +
+            //        $", {item.ModelNumber}" +
+            //        $", {item.ModelDescription}" +
+            //        $", {item.OwnerCompanyName}" +
+            //        $", {item.CompanyCountryCode}" +
+            //        $", {item.CompanyCountryName}" +
+            //        $", {item.BelongsToEU}");
+            //}
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson11_01.IAC/Lesson11_01.IAC/ClassLibrary1/ReportGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using IAC.BL.Repositories;
+
+namespace IAC.BL
+{
+    public class ReportGenerator
+    {
+        private AircraftRepository _aircraftRepository;
+        private AircraftModelRepository _aircraftModelRepository;
+        private CompanyRepository _companyRepository;
+        private CountryRepository _countryRepository;
+
+        public ReportGenerator(
+            AircraftRepository aircraftRepository,
+            AircraftModelRepository aircraftModelRepository,
+            CompanyRepository companyRepository,
+            CountryRepository countryRepository)
+        {
+            _aircraftRepository = aircraftRepository;
+            _aircraftModelRepository = aircraftModelRepository;
+            _companyRepository = companyRepository;
+            _countryRepository = countryRepository;
+        }
+
+        public List<ReportItem> GenerateReportAircraftInEurope()
+        {
+            List<ReportItem> reportItems = new List<ReportItem>();
+
+            foreach (var aircraft in _aircraftRepository.Retrieve())
+            {
+                Company company = _companyRepository.Retrieve(aircraft.CompanyId);
+                Country country = _countryRepository.Retrieve(company.CountryId);
+                AircraftModel aircraftModel = _aircraftModelRepository.Retrieve(aircraft.ModelId);
+
+                if(country.Continent == "Europe")
+                {
+                    ReportItem reportItem = new ReportItem();
+
+                    CreateRaportItem(aircraft, company, country, aircraftModel, reportItem);
+
+                    reportItems.Add(reportItem);
+                }
+            }
+
+            return reportItems;
+        }
+
+        private static void CreateRaportItem(Aircraft aircraft, Company company, Country country, AircraftModel aircraftModel, ReportItem reportItem)
+        {
+            reportItem.AircraftTailNumber = aircraft.TailNumber;
+            reportItem.ModelNumber = aircraftModel.Number;
+            reportItem.ModelDescription = aircraftModel.Description;
+            reportItem.OwnerCompanyName = company.Name;
+            reportItem.CompanyCountryCode = country.Code;
+            reportItem.CompanyCountryName = country.Name;
+            reportItem.BelongsToEU = country.BelongsToEU;
+        }
+    }
+}
